bb3e914e2e8a7368793c52476458d8ef92d92129
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-05-30 18:08:51 +0200

    Add actions to json response

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) =>
+  res.json({
+    actions: [
+      { type: "UNLOCK", time: 1558985702333, status: "PENDING" },
+      { type: "LOCK", time: 1558985698238, status: "FAILED" },
+      { type: "UNLOCK", time: 1557945991480, status: "COMPLETE" },
+      { type: "LOCK", time: 1557945949433, status: "COMPLETE" }
+    ]
+  })
+);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
