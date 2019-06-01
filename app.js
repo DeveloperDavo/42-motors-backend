77e4fa0a940cae90d96dfe79cd555493abbf6169
Developer Davo <DeveloperDavo@users.noreply.github.com>
2019-06-01 13:44:04 +0200

    Make data more realistic

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.get("/actions", (req, res) =>
   res.json({
     actions: [
-      { type: "UNLOCK", time: 1558985702333, status: "PENDING" },
+      { type: "LOCK", time: 1558985702333, status: "PENDING" },
       { type: "LOCK", time: 1558985698238, status: "FAILED" },
       { type: "UNLOCK", time: 1557945991480, status: "COMPLETE" },
       { type: "LOCK", time: 1557945949433, status: "COMPLETE" }
